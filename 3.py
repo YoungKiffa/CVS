d51c9bd22efa111f326e774672948079e0f647c6
YoungKiffa <144680295+YoungKiffa@users.noreply.github.com>
2024-02-12 11:57:24 +0900

    3.py

--- 3.py
+++ 3.py
@@ -1,4 +1,6 @@
 import sys
+
 n, m = map(int, input().split())
-with open('exam.csv', 'w', encoding='utf=8') as file:
+
+with open('exam.csv', 'w', encoding='utf-8') as file:
     file.write("Фамилия;имя;результат 1;результат 2;результат 3;сумма\n")
